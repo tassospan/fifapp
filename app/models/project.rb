c3ba0cdf1471194eaaac68966b5483fc587bf2fb
fifart <fifart@gmail.com>
2016-01-07 13:20:00 +0000

    validation changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
     has_attached_file :image, styles: { medium: "300x300>", thumb: "200x200>" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    validates_presence_of :title, :url
 end
